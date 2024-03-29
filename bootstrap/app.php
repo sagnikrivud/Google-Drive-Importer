a971caa38df3644e3368313c1d621dcdc8396860
sagnikcapital <sagnikd@capitalnumbers.com>
2024-03-29 16:19:12 +0530

    redis

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -100,6 +100,7 @@ $app->register(App\Providers\EventServiceProvider::class);
 $app->register(\Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class);
 $app->register(Illuminate\Mail\MailServiceProvider::class);
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
+$app->register(Illuminate\Database\Eloquent\LegacyFactoryServiceProvider::class);
 
 $app->alias('mail.manager', Illuminate\Mail\MailManager::class);
 $app->alias('mail.manager', Illuminate\Contracts\Mail\Factory::class);

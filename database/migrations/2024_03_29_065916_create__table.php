aed18e14bb41193071ef560a55d421b544ae3fcd
sagnikcapital <sagnikd@capitalnumbers.com>
2024-03-29 17:48:38 +0530

    redis

--- database/migrations/2024_03_29_065916_create__table.php
+++ database/migrations/2024_03_29_065916_create__table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('queue')->index();
             $table->longText('payload');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('');
+        Schema::dropIfExists('jobs');
     }
 };
